6ecb6260058ff11a20b2bbb5167fdaf014dd1478
Georg Zoeller <georgzoeller@users.noreply.github.com>
2023-06-14 01:11:06 +0800

    Update createFlipbook.js

--- scripts/client/createFlipbook.js
+++ scripts/client/createFlipbook.js
@@ -2,6 +2,7 @@ function createScript()
 {
   return {
     description: "Create a flipbook from the current chat",
+    title: "Create Flipboard",
     exec(args){
 
       let images = [];
@@ -12,10 +13,22 @@ function createScript()
             }))
           }
         })
-
-        window.open(`./extensions/omni-extension-flipbook/?images=${encodeURIComponent(JSON.stringify(images))}`, '_blank', 'popup=1,toolbar=0,location=0,menubar=0');
-
+        window.client.workbench.activeExtensionUrl=`./extensions/omni-extension-flipbook/?images=${encodeURIComponent(JSON.stringify(images))}`;
+        window.client.workbench.activeScreen= "extensions";
+        //window.open(`./extensions/omni-extension-flipbook/?images=${encodeURIComponent(JSON.stringify(images))}`, '_blank', 'popup=1,toolbar=0,location=0,menubar=0');
+        window.client.sendSystemMessage(`Flipbook created, please check the extensions tab`, "text/markdown",
+        {
+          commands:
+          [
+            {
+              title: 'Show Flipbook',
+              id: 'toggleExtensions',
+              args: []
+            }
+          ]
+        });
         return {response: "ok"};
     }
   }
 }
+
